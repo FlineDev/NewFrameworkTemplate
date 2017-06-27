d8a0092f3c88503da905d19f2489c8552a6d584e
Cihat Gündüz <CihatGuenduez@posteo.de>
2017-06-27 12:40:20 +0200

    Cleanup public .h file

--- Sources/Supporting Files/NewFrameworkTemplate.h
+++ Sources/Supporting Files/NewFrameworkTemplate.h
@@ -6,27 +6,6 @@
 //  Copyright © 2017 Flinesoft. All rights reserved.
 //
 
-#ifndef NewFrameworkTemplate_h
-#define NewFrameworkTemplate_h
-
-
-#endif /* NewFrameworkTemplate_h */
-
-
-
-
-
-
-
-
-//
-//  NewFrameworkTemplate.h
-//  NewFrameworkTemplate
-//
-//  Created by Cihat Gündüz on 27.06.17.
-//  Copyright © 2017 Flinesoft. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 //! Project version number for NewFrameworkTemplate.
@@ -36,5 +15,4 @@ FOUNDATION_EXPORT double NewFrameworkTemplateVersionNumber;
 FOUNDATION_EXPORT const unsigned char NewFrameworkTemplateVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <NewFrameworkTemplate/PublicHeader.h>
-
 
